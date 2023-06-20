0ae8f18d6ae50fe448c49f8edc498560144ff688
DesmondJombo <desmondomondi453@gmail.com>
2023-06-20 13:55:42 -0700

    task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_alphabet_x10(void);
+int _islower(int c);
 #endif
 
