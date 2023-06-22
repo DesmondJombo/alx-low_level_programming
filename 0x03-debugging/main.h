ed68367aebb7ee30f9dc7af7a8b1c54abe8fbfa1
DesmondJombo <desmondomondi453@gmail.com>
2023-06-22 16:11:14 -0700

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,4 +3,6 @@
 #include <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+takes a date and prints how many days are left in the year, taking
+leap years into account
 #endif
